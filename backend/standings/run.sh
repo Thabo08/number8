39e6e55eb1716fe7025587bc38726d1c2800253c
Thabo Ntsoko <thabo.ginger@gmail.com>
2021-08-04 15:54:22 +0200

    Refactoring the docker file and the start up script

--- backend/standings/run.sh
+++ backend/standings/run.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-export PYTHONPATH="${PYTHONPATH}:/home/"
-export PYTHONPATH="${PYTHONPATH}:/home/backend/"
-export PYTHONPATH="${PYTHONPATH}:/home/backend/standings/"
-cd /home/backend/standings
-export FLASK_APP=main.py
-exec gunicorn -b :5000 --access-logfile - --error-logfile - main:app
+export PYTHONPATH="${PYTHONPATH}:/app/"
+export PYTHONPATH="${PYTHONPATH}:/app/backend/"
+export PYTHONPATH="${PYTHONPATH}:/app/backend/standings/"
+python -m pytest -v
+working_dir="/app/backend/standings"
+if cd $working_dir;
+then
+  export FLASK_APP=main.py
+  exec gunicorn -b :5000 --access-logfile - --error-logfile - main:app
+else
+  echo "$working_dir does not exist"
+  return 1
+fi
